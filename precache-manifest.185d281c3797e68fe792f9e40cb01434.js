196d88854c4e957ea4f0e22343699eb191b0c1e0
Gklam <khalid.lamsadi@outlook.fr>
2020-05-20 04:55:10 +0000

    Updates

---
+++ precache-manifest.185d281c3797e68fe792f9e40cb01434.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "106427c9e5823b2e385b052677499d4e",
+    "url": "/ReactMovieSearchApp/index.html"
+  },
+  {
+    "revision": "99f9a22bd947d0e2a59e",
+    "url": "/ReactMovieSearchApp/static/js/2.548fa1c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactMovieSearchApp/static/js/2.548fa1c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2af3e4c89ce390601198",
+    "url": "/ReactMovieSearchApp/static/js/main.44013d8d.chunk.js"
+  },
+  {
+    "revision": "d1e7cba6a6a916dbcf5b",
+    "url": "/ReactMovieSearchApp/static/js/runtime-main.932c5c62.js"
+  }
+]);
